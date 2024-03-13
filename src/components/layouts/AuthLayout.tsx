cfea738fc5c723fbb7b07ee43780579da9e36c42
Raphael <raphaelcr.ads@gmail.com>
2024-03-13 23:55:29 +0000

    refactor: change auth layout navbar

--- src/components/layouts/AuthLayout.tsx
+++ src/components/layouts/AuthLayout.tsx
@@ -26,25 +26,25 @@ const AuthLayout = () => {
 
     return (
         <>
-            <header className='sticky top-0 z-50 w-full py-2 px-3 flex items-center border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
-                <div className="flex items-center space-x-2">
-                    <Link to='/painel'>
-                        <Icons.logo className="stroke-indigo-500 w-6 h-6" />
-                    </Link>
-                </div>
-                <div className="ml-auto flex items-center space-x-2">
-                    <Label>
-                        {currentUser?.email}
-                    </Label>
-                    <Button variant="ghost" onClick={doLogout}>
-                        <Icons.logout className='h-4 w-4 mr-2' />
-                        Sair
-                    </Button>
+            <header className='sticky top-0 z-50 py-2 border-b bg-white'>
+                <div className="flex w-full items-center container">
+                    <div className="flex items-center space-x-2 ml-2">
+                        <Link to='/painel'>
+                            <Icons.logo className="stroke-indigo-500 w-6 h-6" />
+                        </Link>
+                    </div>
+                    <div className="ml-auto flex items-center space-x-2 mr-2">
+                        <Label>
+                            {currentUser?.email}
+                        </Label>
+                        <Button variant="ghost" onClick={doLogout}>
+                            <Icons.logout className='h-4 w-4 mr-2' />
+                            Sair
+                        </Button>
+                    </div>
                 </div>
             </header>
-            <section className='p-2'>
-                <Outlet />
-            </section>
+            <Outlet />
         </>
     )
 }
