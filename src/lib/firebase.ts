683c3ce3023f0ae1b2fc2c4435b5d7bfe645c073
Raphael <raphaelcr.ads@gmail.com>
2024-03-13 22:39:30 +0000

    feat: add firebase storage

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
+import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: "AIzaSyBeEyVFtXLpZpdndzO1hTIC-xMG_-yIqio",
@@ -14,5 +15,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 const auth = getAuth(app)
+const storage = getStorage(app)
 
-export { app, db, auth }
+export { app, db, auth, storage }
