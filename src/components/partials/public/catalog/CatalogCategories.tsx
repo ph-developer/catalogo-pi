0230b827052641c9b01b239ae5387a7324b95699
Raphael <raphaelcr.ads@gmail.com>
2024-03-21 06:25:33 -0300

    refactor: hide available categories with no products on CatalogCategories partial component

--- src/components/partials/public/catalog/CatalogCategories.tsx
+++ src/components/partials/public/catalog/CatalogCategories.tsx
@@ -2,6 +2,7 @@ import { Category } from "@/types/category"
 import { Product } from "@/types/product"
 import {Catalog} from "@/types/catalog";
 import {colors} from "@/lib/colors.ts";
+import {useMemo} from "react";
 
 interface Props {
     catalog: Catalog
@@ -11,6 +12,15 @@ interface Props {
 }
 
 export const CatalogCategories = ({ catalog, categories, products, onCategoryClick }: Props) => {
+    const categoriesDisplay = useMemo(() => {
+        return categories.map((category: Category) => ({
+            category,
+            productsCount: products
+                .filter((product: Product) => product.categoryIds.includes(category.id!))
+                .length
+        })).filter(({productsCount}) => productsCount > 0)
+    }, [categories, products])
+
     return (
         <div style={catalog?.bannerDominantColor ? {
             color: colors.getTextColor(catalog.bannerDominantColor),
@@ -18,17 +28,13 @@ export const CatalogCategories = ({ catalog, categories, products, onCategoryCli
             <p className="mb-1 text-sm font-medium">
                 Categorias
             </p>
-            {categories.map((category) => (
+            {categoriesDisplay.map(({category, productsCount}) => (
                 <p
                     key={category.id}
                     className="text-sm cursor-pointer"
                     onClick={() => onCategoryClick(category)}
                 >
-                    {category.name} ({
-                        products
-                            .filter((p) => p.categoryIds.includes(category.id!))
-                            .length
-                    })
+                    {category.name} ({productsCount})
                 </p>
             ))}
         </div>
