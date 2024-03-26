a2aca18cb83d51d5f95b3850db250f741dda796c
Raphael <raphaelcr.ads@gmail.com>
2024-03-26 19:24:25 -0300

    build: split firebase vendor bundle into smaller chunks (firebase and firebase-data)

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,40 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import {visualizer} from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [
+        react(),
+        visualizer()
+    ],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    'firebase': [
+                        'firebase/app',
+                        'firebase/auth',
+                        'firebase/storage'
+                    ],
+                    'firebase-data': [
+                        'firebase/firestore',
+                        'firebase/database',
+                    ]
+                }
+            },
+        }
     },
-  },
-  server: {
-    port: 3000,
-    watch: {
-      usePolling: true,
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+    server: {
+        port: 3000,
+        watch: {
+            usePolling: true,
+        }
     }
-  }
 })
